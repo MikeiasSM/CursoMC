fdc056c129ca58a03781f3de0fae61efd070fd81
Mikeias Santos <mikeiassantos16@gmail.com>
2019-09-03 12:55:40 -0300

    subtotal e total do pedido

--- src/main/java/br/com/mikeiassantos/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/com/mikeiassantos/cursomc/repositories/ProdutoRepository.java
@@ -5,9 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.mikeiassantos.cursomc.domain.Categoria;
 import br.com.mikeiassantos.cursomc.domain.Produto;
@@ -15,6 +14,7 @@ import br.com.mikeiassantos.cursomc.domain.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+	@Transactional(readOnly = true)
 	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
