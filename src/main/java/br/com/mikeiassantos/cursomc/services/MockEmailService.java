c3a3dbac2d154c0b08fbe735add5f88dc440d581
Mikeias Santos <mikeiassantos16@gmail.com>
2019-09-16 07:45:38 -0300

    Email HTML

--- src/main/java/br/com/mikeiassantos/cursomc/services/MockEmailService.java
+++ src/main/java/br/com/mikeiassantos/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package br.com.mikeiassantos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,7 +12,15 @@ public class MockEmailService extends AbstractEmailServices {
 	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
-		LOG.info("Enviando Email...");
+		LOG.info("Simulando Enviando Email...");
+		LOG.info(msg.toString());
+		LOG.info("Email Enviado!");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando Enviando Email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email Enviado!");
 		
