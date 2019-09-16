a3a9df3c78afb3779e57ba5a758cd89e7b2c06a1
Mikeias Santos <mikeiassantos16@gmail.com>
2019-09-16 04:15:58 -0300

    Novo envio de aplicação para heroku

--- src/main/java/br/com/mikeiassantos/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/mikeiassantos/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.mikeiassantos.cursomc.domain.Categoria;
 import br.com.mikeiassantos.cursomc.domain.Pedido;
-import br.com.mikeiassantos.cursomc.dto.CategoriaDTO;
 import br.com.mikeiassantos.cursomc.services.PedidoService;
 
 @RestController
